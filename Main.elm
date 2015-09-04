4af9092129066ff3146ab6eb53d16c382e0346af
Amitai Burstein <amitai@gizra.com>
2015-09-04 23:52:58 +0300

    Initial commit

---
+++ Main.elm
@@ -0,0 +1,23 @@
+
+import Effects exposing (Never)
+import Login exposing (init, update, view)
+import StartApp
+import Task
+
+
+app =
+  StartApp.start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+
+main =
+  app.html
+
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks

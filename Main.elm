c1e722c468e10c4f99946b3e8d98441ed17a7180
Amitaibu <amitai@gizra.com>
2015-09-06 15:01:04 +0300

    Add User module

--- Main.elm
+++ Main.elm
@@ -1,15 +1,16 @@
 
 import Effects exposing (Never)
 import Login exposing (init, update, view)
+import User exposing (init, update, view)
 import StartApp
 import Task
 
 
 app =
   StartApp.start
-    { init = init
-    , update = update
-    , view = view
+    { init = User.init
+    , update = User.update
+    , view = User.view
     , inputs = []
     }
 

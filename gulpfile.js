502a5ac52e0415ec5351ac40a4d7aa336564f3dd
Amitai Burstein <amitai@gizra.com>
2015-11-27 23:20:15 +0200

    Add more GA options to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -128,7 +128,12 @@ gulp.task('bower', function () {
 
 gulp.task('ga', function(){
   gulp.src('src/index.html')
-  .pipe(ga({url: 'gizra.github.io', uid: 'UA-6558346-13'}))
+  .pipe(ga({
+    anonymizeIp: false,
+    sendPageView: true,
+    url: 'auto',
+    uid: 'UA-6558346-13'
+  }))
   .pipe(gulp.dest('serve'));
 });
 

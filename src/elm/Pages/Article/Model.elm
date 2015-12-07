b17c30c1acbf8218cb044a3f68273be7363fb2ad
Amitaibu <amitai@gizra.com>
2015-12-07 14:38:31 +0200

    Revert "Add ports to page level'"
    This reverts commit 3ff14f4ec80eab7a000dcf33fdae475b0def5620.

--- src/elm/Pages/Article/Model.elm
+++ src/elm/Pages/Article/Model.elm
@@ -3,26 +3,13 @@ module Pages.Article.Model where
 import ArticleForm.Model as ArticleForm exposing (initialModel, Model)
 import ArticleList.Model as ArticleList exposing (initialModel, Model)
 
-type alias Ports =
-  { dropzoneUploadedFile: Maybe Int
-  , ckeditor : String
-  }
-
 type alias Model =
   { articleForm : ArticleForm.Model
   , articleList : ArticleList.Model
-  , ports : Ports
-  }
-
-initialPorts : Ports
-initialPorts =
-  { dropzoneUploadedFile = Nothing
-  , ckeditor = ""
   }
 
 initialModel : Model
 initialModel =
   { articleForm = ArticleForm.initialModel
   , articleList = ArticleList.initialModel
-  , ports = initialPorts
   }

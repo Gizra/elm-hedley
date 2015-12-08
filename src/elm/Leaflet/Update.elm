043d5e9d03c4056c55d91491cb87098462adb6e6
Amitaibu <amitai@gizra.com>
2015-12-08 12:58:05 +0200

    Convert Leaflet to a proper component

---
+++ src/elm/Leaflet/Update.elm
@@ -0,0 +1,35 @@
+module Leaflet.Update where
+
+import Leaflet.Model exposing (initialModel, Model)
+
+import Effects exposing (Effects)
+
+init : (Model, Effects Action)
+init =
+  ( initialModel
+  , Effects.none
+  )
+
+type Action
+  = ToggleMap
+  | SelectMarker (Maybe Int)
+  | UnselectMarker
+
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+  case action of
+    ToggleMap ->
+      ( { model | showMap <- (not model.showMap) }
+      , Effects.none
+      )
+
+    SelectMarker val ->
+      ( { model | selectedMarker <- val }
+      , Effects.none
+      )
+
+    UnselectMarker ->
+      ( { model | selectedMarker <- Nothing }
+      , Effects.none
+      )

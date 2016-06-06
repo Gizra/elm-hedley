f374a3b1680b16d1d3b0f722a74c7c82f28b4f6a
Amitaibu <amitai@gizra.com>
2016-06-06 14:44:42 +0300

    Update DEV

--- src/elm/Config.elm
+++ src/elm/Config.elm
@@ -5,7 +5,7 @@ import Time exposing (Time)
 
 localBackend : BackendConfig
 localBackend =
-  { backendUrl = "https://dev-hedley-elm.pantheon.io"
+  { backendUrl = "https://dev-hedley-elm.pantheonsite.io"
   , githubClientId = "e5661c832ed931ae176c"
   , name = "local"
   , hostname = "localhost"

02cc20a3a1e12ef9ade595e80111090119ba8fab
Amitai Burstein <amitai@gizra.com>
2016-06-07 21:21:26 +0300

    Remove old vendor packages

--- src/vendor/elm-param-parsing/test.sh
+++
@@ -1,4 +0,0 @@
-elm-make src/*.elm
-elm-make test/*.elm --output test/raw-test.js
-bash test/elm-io.sh test/raw-test.js test/test.js
-node test/test.js
--- src/vendor/elm-web-api/test/src/elm/TestMailbox.elm
+++
@@ -1,9 +0,0 @@
-module TestMailbox where
-
-import Signal exposing (Signal, Mailbox, mailbox, constant, send)
-import ElmTest.Test exposing (Test, suite)
-
-
-tests : Mailbox Test
-tests =
-    mailbox (suite "Tests have not arrived yet" [])
